76b044aa8958eeef3d5f6050dfc25a9e7187e69b
BabatundeAlo <babatunde.olabode@oryx-wa.com>
2018-07-24 23:42:35 +0800

    Initial upload

---
+++ OryxCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OryxCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
